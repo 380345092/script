22a205b1fb05714f35794bb727e34cb6458229e7
luwei <380345092@qq.com>
2022-04-09 20:27:32 +0800

    Create fdisk.sh

---
+++ shell/fdisk.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+fdisk /dev/sdc << EOF
+n
+p
+
+
+
+w
+EOF
+mkfs.xfs /dev/sdc1
+mkdir /data01
+mount /dev/sdc1 /data01
+echo /dev/sdc1                                 /data01                    xfs    defaults        0 0 >> /etc/fstab
+
